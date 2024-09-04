16cc6f8ab90619b27b961347bf839ead6c08e57d
Akhil Chainani <akhil.chainani@smartcontract.com>
2024-09-04 13:53:54 -0500

    use generic read from file function

--- tools/signing/io.go
+++ tools/signing/io.go
@@ -8,29 +8,40 @@ import (
 	"github.com/smartcontractkit/ccip-owner-contracts/tools/proposal/timelock"
 )
 
-func ProposalFromFile(filePath string) (*mcms.Proposal, error) {
-	var out mcms.Proposal
-
+// Generic function to read a file and unmarshal its contents into the provided struct
+func FromFile(filePath string, out interface{}) error {
 	// Load file from path
 	fileBytes, err := os.ReadFile(filePath)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	json.Unmarshal(fileBytes, out)
+	// Unmarshal JSON into the provided struct
+	err = json.Unmarshal(fileBytes, out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Usage for mcms.Proposal
+func ProposalFromFile(filePath string) (*mcms.Proposal, error) {
+	var out mcms.Proposal
+	err := FromFile(filePath, &out)
+	if err != nil {
+		return nil, err
+	}
 	return &out, nil
 }
 
+// Usage for timelock.MCMSWithTimelockProposal
 func TimelockProposalFromFile(filePath string) (*timelock.MCMSWithTimelockProposal, error) {
 	var out timelock.MCMSWithTimelockProposal
-
-	// Load file from path
-	fileBytes, err := os.ReadFile(filePath)
+	err := FromFile(filePath, &out)
 	if err != nil {
 		return nil, err
 	}
-
-	json.Unmarshal(fileBytes, out)
 	return &out, nil
 }
 
